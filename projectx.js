e1dafefcbc017f0935df8adb77e0d03c35d06d46
Abdulazeez Saidu <66853129+Danmasanii@users.noreply.github.com>
2020-07-16 15:34:36 +0100

    Add files via upload

---
+++ projectx.js
@@ -0,0 +1,13 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
